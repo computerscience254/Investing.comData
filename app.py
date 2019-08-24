664c79365b9c2d356437a343e011d4cec4ed618b
Rodney Osodo <blackd0t@protonmail.com>
2019-08-24 11:51:12 +0300

    Adding flask graphql api application

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask_graphql import GraphQLView
+from flask import Flask
+from models import db_session, Base, engine
+from schema import schema
+
+app = Flask(__name__)
+
+# Db configs
+
+#  Schema Objects
+
+#  Routes
+
+app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True, context={"session": db_session}))
+
+@app.route('/')
+def index():
+    return "Go to /graphql"
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
+if __name__ == '__main__':
+    Base.metadata.create_all(bind=engine)
+    app.debug=True
+    app.run()
