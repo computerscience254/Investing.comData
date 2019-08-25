26651321552f471f231fc64fdd2f1a38d33d9547
Rodney Osodo <blackd0t@protonmail.com>
2019-08-25 13:47:17 +0300

    Adding config

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
-basedir = os.path.abspath(os.path.curdir)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+
